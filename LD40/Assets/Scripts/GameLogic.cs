569bbceb9bad37b0806e67c74b3c14c9f338bcbe
Unknown <creativewhisperings@gmail.com>
2017-12-02 10:19:01 +0100

    Initial GameLogic Setup

---
+++ LD40/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+    public bool ActionProcessing;
+    public bool HashtagActive;
+
+    Slider progressBar;
+    GameState gState;
+
+    private void Awake()
+    {
+        progressBar = GameObject.Find("ProgressBar").GetComponent<Slider>();
+    }
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(TimeUpdate());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator TimeUpdate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            UpdateCounters();
+        }
+    }
+
+    void UpdateCounters()
+    {
+        Debug.Log("Turn Processed");
+    }
+
+    void Photo()
+    {
+
+    }
+
+    void Video()
+    {
+
+    }
+
+    void Tweet()
+    {
+
+    }
+
+    void LongPost()
+    {
+
+    }
+}
+
--- LD40/Assets/Scripts/GameState.cs
+++ LD40/Assets/Scripts/GameState.cs
@@ -1,13 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameState : MonoBehaviour {
 
     public int Health = 100;
     public int Followers = 1;
+    public int Money = 1000;
 
+    // Keep them serialized for debugging, hide them on final project
+    [SerializeField]
+    Text healthText;
+    [SerializeField]
+    Text followersText;
+    [SerializeField]
+    Text moneyText;
 
+    private void Awake()
+    {
+        healthText = GameObject.Find("Health").GetComponent<Text>();
+        followersText = GameObject.Find("Followers").GetComponent<Text>();
+        moneyText = GameObject.Find("Money").GetComponent<Text>();
+    }
 
+    private void Update()
+    {
+        // Game Over controller
+        if(Health <= 0)
+        {
+            YouKillYourself();
+        } else if (Money <= 0)
+        {
+            YouStarveToDeath();
+        } else
+        {
+            return;
+        }
+    }
+
+    void YouKillYourself()
+    {
+        Debug.Log("You Killed Yourself due to stress");
+    }
+
+    void YouStarveToDeath()
+    {
+        Debug.Log("You are broke and die from inanition");
+    }
 }
 
