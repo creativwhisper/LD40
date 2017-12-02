72623d7b155eb1cb33a870df8edabf3f73053680
Unknown <creativewhisperings@gmail.com>
2017-12-02 09:43:54 +0100

    UI mockup layout and gamestate script

---
+++ LD40/Assets/Scripts/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    public int Health = 100;
+    public int Followers = 1;
+
+
+
+}
+
