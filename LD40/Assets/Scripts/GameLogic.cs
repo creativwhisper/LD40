8bc22c73651a003a4b22a9606252eb6c8cdbdd57
Unknown <creativewhisperings@gmail.com>
2017-12-03 17:22:40 +0100

    GameBalance and Social Media Posts Logic

--- LD40/Assets/Scripts/GameLogic.cs
+++ LD40/Assets/Scripts/GameLogic.cs
@@ -17,6 +17,16 @@ public class GameLogic : MonoBehaviour {
     public string[] NegativePhrases;
     public string[] VeryPossitivePhrases;
     public string[] VeryNegativePhrases;
+    public Text Reaction1;
+    public Text Reaction2;
+    public Text Reaction3;
+    public Text Reaction4;
+    public Text Reaction5;
+    public GameObject PhotoGO;
+    public GameObject TweetGO;
+    public GameObject VideoGO;
+    public GameObject PostGO;
+
 
     [SerializeField]
     ParticleSystem thumbUpParticle;
@@ -64,19 +74,38 @@ public class GameLogic : MonoBehaviour {
 
     void UpdateCounters()
     {
-        PlayerHasReactedThisTurn = false;
+        
         gState.Money -= newMoneyDelta;
-        newMoneyDelta += 10;
+        if (TurnNumber < 10)
+        {
+            newMoneyDelta += 10;
+        } else if (TurnNumber >= 10 && TurnNumber < 30)
+        {
+            newMoneyDelta += 30;
+        } else if (TurnNumber >= 30)
+        {
+            newMoneyDelta += 50;
+        }
+
         if (!PlayerHasReactedThisTurn)
         {
-                    
-            gState.Followers = (gState.Followers / 2) + 1;
+            if (gState.Followers < 1000)
+            {
+                gState.Followers -= ((gState.Followers * 10) / 100) + 1;
+            } else if (gState.Followers >= 1000 && gState.Followers < 100000)
+            {
+                gState.Followers -= ((gState.Followers * 25) / 100) + 50;
+            } else
+            {
+                gState.Followers -= ((gState.Followers * 40) / 100) + 1000;
+            }
         }
         else
         {
             gState.Followers += ((gState.Followers * 5) / 100) + 1;
         }
-
+        PlayerHasReactedThisTurn = false;
+        gState.Money += (gState.Followers / 20) + 1;
         // Debug.Log("Turn Processed");
         // Debug.Log(TurnNumber);
     }
@@ -84,70 +113,176 @@ public class GameLogic : MonoBehaviour {
     public void Photo()
     {
         ActionSelected = true;
-        
+        VideoGO.SetActive(false);
+        TweetGO.SetActive(false);
+        PostGO.SetActive(false);
+        PhotoGO.SetActive(true);
     }
 
     public void Video()
     {
         ActionSelected = true;
-        
+        VideoGO.SetActive(true);
+        TweetGO.SetActive(false);
+        PostGO.SetActive(false);
+        PhotoGO.SetActive(false);
     }
 
     public void Tweet()
     {
         ActionSelected = true;
-        
+        VideoGO.SetActive(false);
+        TweetGO.SetActive(true);
+        PostGO.SetActive(false);
+        PhotoGO.SetActive(false);
     }
 
     public void LongPost()
     {
         ActionSelected = true;
-        
+        VideoGO.SetActive(false);
+        TweetGO.SetActive(false);
+        PostGO.SetActive(true);
+        PhotoGO.SetActive(false);
     }
 
     public void CorrectMood()
     {
-        Debug.Log("Correct!");
+        //Debug.Log("Correct!");
         HashtagActive = false;
         ActionSelected = false;
-        gState.Followers += ((gState.Followers * 10) / 100) + 1;
+        if (gState.Followers < 1000)
+        {
+            gState.Followers += ((gState.Followers * 20) / 100) + 10;
+        } else if (gState.Followers > 1001 && gState.Followers < 100000){
+            gState.Followers += ((gState.Followers * 15) / 100) + 100;
+        } else
+        {
+            gState.Followers += ((gState.Followers * 10) / 100) + 200;
+        }
         thumbUpParticle.Play();
+        if (gState.Health < 50 || gState.Followers > 100000)
+        {
+            Reaction1.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction2.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction3.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction4.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction5.text = VeryPossitivePhrases[Random.Range(0, 15)];
+        }
+        else
+        {
+            Reaction1.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction2.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction3.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction4.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction5.text = PossitivePhrases[Random.Range(0, 15)];
+        }
     }
 
     public void IncorrectMood()
     {
-        Debug.Log("IncorrectMood!");
+        //Debug.Log("IncorrectMood!");
         HashtagActive = false;
         ActionSelected = false;
-        gState.Followers -= ((gState.Followers * 10) / 100) + 1;
+        if (gState.Followers <= 1000)
+        {
+            gState.Followers -= ((gState.Followers * 10) / 100) + 1;
+        }
+        else if (gState.Followers > 1001 && gState.Followers < 100000)
+        {
+            gState.Followers -= ((gState.Followers * 15) / 100) + 50;
+        }
+        else
+        {
+            gState.Followers -= ((gState.Followers * 20) / 100) + 100;
+        }
+        
         thumbDownParticle.Play();
+        if (gState.Health < 50 || gState.Followers > 100000)
+        {
+            Reaction1.text = VeryNegativePhrases[Random.Range(0, 15)];
+            Reaction2.text = VeryNegativePhrases[Random.Range(0, 15)];
+            Reaction3.text = VeryNegativePhrases[Random.Range(0, 15)];
+            Reaction4.text = VeryNegativePhrases[Random.Range(0, 15)];
+            Reaction5.text = VeryNegativePhrases[Random.Range(0, 15)];
+        }
+        else
+        {
+            Reaction1.text = NegativePhrases[Random.Range(0, 15)];
+            Reaction2.text = NegativePhrases[Random.Range(0, 15)];
+            Reaction3.text = NegativePhrases[Random.Range(0, 15)];
+            Reaction4.text = NegativePhrases[Random.Range(0, 15)];
+            Reaction5.text = NegativePhrases[Random.Range(0, 15)];
+        }
     }
 
-    /*public void FakeMood()
+    public void FakeMood()
     {
+        HashtagActive = false;
+        ActionSelected = false;
         gState.Health -= 10;
-        
+        gState.Followers += ((gState.Followers * 10) / 100) + 1;
+        if (gState.Health < 50 || gState.Followers > 100000)
+        {
+            Reaction1.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction2.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction3.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction4.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction5.text = VeryPossitivePhrases[Random.Range(0, 15)];
+        }
+        else
+        {
+            Reaction1.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction2.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction3.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction4.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction5.text = PossitivePhrases[Random.Range(0, 15)];
+        }
+        thumbUpParticle.Play();
     }
 
     public void PersonalPost()
     {
+        thumbUpParticle.Play();
+        HashtagActive = false;
+        ActionSelected = false;
         gState.Health -= 15;
         // StartCoroutine(MultipliyingFollowers());
-        gState.Followers += (gState.Followers * 50) / 100;
-    }
-
-    IEnumerator MultipliyingFollowers()
-    {
-        
-        gState.Followers += (gState.Followers * 50) / 100;
-        for (int i = 0; i < 4; i++)
+        if (gState.Followers < 1000)
         {
-            yield return new WaitForSeconds(1f);
             gState.Followers += (gState.Followers * 50) / 100;
-            
+            gState.Followers += (gState.Followers * 50) / 100;
+            gState.Followers += (gState.Followers * 50) / 100;
         }
-        yield return null;
-            
-    }*/
+        else if (gState.Followers > 1001 && gState.Followers < 100000)
+        {
+            gState.Followers += (gState.Followers * 40) / 100;
+            gState.Followers += (gState.Followers * 30) / 100;
+            gState.Followers += (gState.Followers * 20) / 100;
+        }
+        else
+        {
+            gState.Followers += (gState.Followers * 30) / 100;
+            gState.Followers += (gState.Followers * 20) / 100;           
+        }
+        if (gState.Health < 50 || gState.Followers > 100000)
+        {
+            Reaction1.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction2.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction3.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction4.text = VeryPossitivePhrases[Random.Range(0, 15)];
+            Reaction5.text = VeryPossitivePhrases[Random.Range(0, 15)];
+        }
+        else
+        {
+            Reaction1.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction2.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction3.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction4.text = PossitivePhrases[Random.Range(0, 15)];
+            Reaction5.text = PossitivePhrases[Random.Range(0, 15)];
+        }
+        
+    }
+    
 }
 
